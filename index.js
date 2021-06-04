cf8c427d0dcbe32e85fd54658a48b52fd62dda32
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-04 16:05:23 +0600

    using module in project

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url')
 
+const replaceTemplate = require('./modules/replaceTemplate')
+
 // fs.readFile('./starter/txt/start.txt','utf8',(err,data1)=>{
 //     if(err) return console.error('Error!!!')
 //     fs.readFile(`./starter/txt/${data1}.txt`,'utf8',(err,data2)=>{
@@ -17,20 +19,7 @@ const url = require('url')
 // })
 
 // console.log('I am reading....')
-const replaceTemplate = (temp,product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName)
-    output = output.replace(/{%IMAGE%}/g,product.image)
-    output = output.replace(/{%FROM%}/g,product.from)
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g,product.quantity)
-    output = output.replace(/{%PRICE%}/g,product.price)
-    output = output.replace(/{%DESCRIPTION%}/g,product.description)
-    output = output.replace(/{%ID%}/g,product.id)
 
-    if(!product.organic) output = output.replace(/{%NOT-ORGANIC%}/g,'not-organic')
-    return output
-    
-}
 const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`,'utf8')
 const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,'utf8')
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`,'utf8')
